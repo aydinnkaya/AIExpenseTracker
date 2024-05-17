a826255f5eed152422a783ab04f8d75765dd0c91
Aydın Kaya <Aydinnkaya1@gmail.com>
2024-05-17 11:08:15 +0300

    FirebaseUpdate
    FirebaseFirestore

--- AIExpenseTrackerApp.swift
+++ AIExpenseTrackerApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct AIExpenseTrackerApp: App {
+    
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    #else
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    #endif
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+            #if os(macOS)
+                .frame(minWidth: 729, minHeight: 480)
+            #endif
         }
+        
+        #if os(macOS)
+        .windowResizability(.contentMinSize)
+        #endif
+        
     }
 }

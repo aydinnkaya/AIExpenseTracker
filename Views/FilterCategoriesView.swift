d50f59ad787e2096686370703a53c9b51cea5c5a
Aydın Kaya <Aydinnkaya1@gmail.com>
2024-05-21 23:53:55 +0300

    FilterCategoriesView
    FilterCategoriesView

--- Views/FilterCategoriesView.swift
+++ Views/FilterCategoriesView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct FilterCategoriesView: View {
+    
+    @Binding var selectedCategories: Set<Category>
+    private let categories = Category.allCases
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ScrollView(.horizontal){
+                HStack(spacing: 8){
+                    ForEach(categories){ category in
+                        FilterButtonView(category: category, isSelected: self.selectedCategories.contains(category), onTab: self.onTab)
+                        
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+    }
+    
+    func onTab(category: Category){
+        if selectedCategories.contains(category){
+            selectedCategories.remove(category)
+        }
+        else {
+            selectedCategories.insert(category)
+        }
+    }
+}
+struct FilterButtonView: View {
+    var category: Category
+    var isSelected: Bool
+    var onTab: (Category) -> ()
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
     }
 }
 
+
 #Preview {
-    FilterCategoriesView()
+    @State var vm = LogListViewModel()
+    
+    return FilterCategoriesView(selectedCategories: $vm.selectedCategories)
 }

ee228b689812f6f7fd5ea6b96bfea70e784492b0
Aydın Kaya <Aydinnkaya1@gmail.com>
2024-05-24 19:44:47 +0300

    LogItemView
    LogItemView, CategoryImageView

--- Models/Sort.swift
+++ Models/Sort.swift
@@ -10,24 +10,24 @@ import Foundation
 enum SortType: String, Identifiable, CaseIterable{
     
     var id: Self { self }
-     case date, amount, name
-     
-     var systemNameIcon: String {
-         switch self {
-         case .date:
-             return "calendar"
-         case .amount:
-             return "dollarsign.circle"
-         case .name:
-             return "a"
-         }
-     }
-     
- }
+    case date, amount, name
+    
+    var systemNameIcon: String {
+        switch self {
+        case .date:
+            return "calendar"
+        case .amount:
+            return "dollarsign.circle"
+        case .name:
+            return "a"
+        }
+    }
+    
+}
 
- enum SortOrder: String, Identifiable, CaseIterable {
-     
-     var id: Self { self }
-     case ascending, descending
-     
- }
+enum SortOrder: String, Identifiable, CaseIterable {
+    
+    var id: Self { self }
+    case ascending, descending
+    
+}
---
+++ Views/LogListView.swift
@@ -0,0 +1,18 @@
+//
+//  LogListView.swift
+//  AIExpenseTracker
+//
+//  Created by Aydın KAYA on 24.05.2024.
+//
+
+import SwiftUI
+
+struct LogListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogListView()
+}
